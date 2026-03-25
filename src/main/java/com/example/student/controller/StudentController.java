8a3e78145f7b4902a673e1e634dd57e69a6ba201
Miantsa <hei.miantsa.3@gmail.com>
2026-03-25 23:18:38 +0300

    TD4

--- src/main/java/com/example/student/controller/StudentController.java
+++ src/main/java/com/example/student/controller/StudentController.java
@@ -1,46 +1,51 @@
 package com.example.student.controller;
-
+import com.example.student.exception.BadRequestException;
+import com.example.student.service.StudentService;
+import com.example.student.validator.StudentValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import com.example.student.entity.Student;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 public class StudentController {
-    private List<Student> students = new ArrayList<>();
+    private final StudentValidator studentValidator = new StudentValidator();
+    private final StudentService studentService = new StudentService();
+
 
     @GetMapping("/welcome")
     public ResponseEntity<String> welcome(
             @RequestParam(required = false) String name) {
-
         if (name == null || name.isBlank()) {
             return ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
                     .body("Paramètre 'name' manquant");
         }
-
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .header("Content-Type", "text/plain")
                 .body("Welcome " + name);
     }
 
+
     @PostMapping("/students")
-    public ResponseEntity<?> addStudents(
+    public ResponseEntity<?> createStudents(
             @RequestBody List<Student> newStudents) {
-
         try {
-            students.addAll(newStudents);
-
+            studentValidator.validateAll(newStudents);
+            List<Student> allStudents = studentService.saveAll(newStudents);
             return ResponseEntity
                     .status(HttpStatus.CREATED)
                     .header("Content-Type", "application/json")
-                    .body(students);
-
+                    .body(allStudents);
+        } catch (BadRequestException e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .header("Content-Type", "text/plain")
+                    .body(e.getMessage());
         } catch (Exception e) {
             return ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -48,41 +53,36 @@ public class StudentController {
         }
     }
 
+
+
     @GetMapping("/students")
     public ResponseEntity<?> getStudents(
             @RequestHeader(value = "Accept", required = false) String accept) {
-
         if (accept == null || accept.isBlank()) {
             return ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
                     .body("Header 'Accept' manquant");
         }
-
         try {
+            List<Student> allStudents = studentService.getAll();
             if (accept.equals("text/plain")) {
-
-                String result = students.stream()
+                String result = allStudents.stream()
                         .map(s -> s.getFirstName() + " " + s.getLastName())
                         .collect(Collectors.joining(", "));
-
                 return ResponseEntity
                         .status(HttpStatus.OK)
                         .header("Content-Type", "text/plain")
                         .body(result);
-
             } else if (accept.equals("application/json")) {
-
                 return ResponseEntity
                         .status(HttpStatus.OK)
                         .header("Content-Type", "application/json")
-                        .body(students);
-
+                        .body(allStudents);
             } else {
                 return ResponseEntity
                         .status(HttpStatus.NOT_IMPLEMENTED)
                         .body("Format non supporté");
             }
-
         } catch (Exception e) {
             return ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
